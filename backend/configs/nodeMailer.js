8863b8b4ccc4746c88529373212f3c1087cacc7e
gowtham_uj <108682700+gowtham-uj@users.noreply.github.com>
2023-01-08 21:47:47 +0530

    setting up for deploy

--- backend/configs/nodeMailer.js
+++ backend/configs/nodeMailer.js
@@ -1,21 +1,21 @@
 const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.ethereal.email",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: "reyna67@ethereal.email",
-//     pass: "4Bp2xHYWk6t4EX8hxd",
-//   },
-// });
 const transporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
   auth: {
-    user: "ae5f578ddac4d0",
-    pass: "1935667c830cf1",
+    user: "reyna67@ethereal.email",
+    pass: "4Bp2xHYWk6t4EX8hxd",
   },
 });
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: "ae5f578ddac4d0",
+//     pass: "1935667c830cf1",
+//   },
+// });
 
 module.exports = transporter;
